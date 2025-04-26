967badfe2861197c8df85012c7391d99857e71db
Daniel Marschall <info@daniel-marschall.de>
2025-04-26 20:57:58 +0000

    _DelWaste.bat
    git-svn-id: https://svn.viathinksoft.com/svn/decoder/trunk@164 02500784-8bdb-4f6e-8fde-60afc101638d

--- Decoder5x/_DelWaste.bat
+++ Decoder5x/_DelWaste.bat
@@ -1,13 +1,17 @@
 @echo off
 cd /d %~dp0
-del bin\*.rsm
-del *.dcu
+del Output\win32\*.rsm
+del Output\win32\*.dcu
+del Output\win64\*.rsm
+del Output\win64\*.dcu
 del *.local
 del *.identcache
 del *.~*
 del *.o
 del *.rsm
+del Translation\*.xlat.$*
 attrib __history -h
 if exist __history rmdir /s /q __history
 attrib __recovery -h
 if exist __recovery rmdir /s /q __recovery
+pause.
